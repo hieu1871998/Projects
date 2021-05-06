8a1a419cfd71828763c98d9540c3f3b6f47632f8
Vi Minh Hiếu <hieu1871998@gmail.com>
2021-05-06 12:15:36 +0700

    Update Array_E1.c

--- 7.Array/Array_E1.c
+++ 7.Array/Array_E1.c
@@ -2,25 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-int compare(const void * a, const void * b)
-{
-	return strcmp(*(char**)a, *(char**)b);
-}
-  
 void main()
 {
-	char* arr[] = { "George", "Albert", "Tina", "Xavier", "Roger", "Tim", "William" };
+	char * arr[] = { "George", "Albert", "Tina", "Xavier", "Roger", "Tim", "William" };
+	char * temp;
 	int n = sizeof(arr) / sizeof(arr[0]);
-	int i;
+	int i, j;
 	printf("Original given array:\n");
 	for(i = 0;i < n;i++)
 	{
 		printf("%d. %s\n",i + 1,arr[i]);
 	}
-	qsort(arr, n, sizeof(arr[0]), compare);
 	printf("\nGiven array sorted:\n");
 	for (i = 0; i < n; i++)
 	{
-		printf("%d: %s \n", i + 1, arr[i]);
+		for(j = i + 1;j < n;j++)
+		{
+			if(strcmp(arr[i],arr[j]) > 0)
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		printf("%d. %s\n",i + 1,arr[i]);
 	}
 }
