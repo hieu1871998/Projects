638d820e252c66db9a4e2439dbebd1488a9076ef
Vi Minh Hiếu <hieu1871998@gmail.com>
2021-05-08 19:46:10 +0700

    Update SideHW1.c

--- SideHW1.c
+++ SideHW1.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pop = 1;
+int pop;
 
-struct student
+typedef struct student
 {
 	char name[50];
 	char province[50];
 	int birthYear;
 	float mark;
-};
-struct student obj[];
+} studentInfo;
+studentInfo *obj;
 
 void inputInfo()
 {
 	int i;
 	printf("Number of students: ");
 	scanf("%d",&pop);
+	obj = (studentInfo*)malloc(pop * sizeof(studentInfo));
 	printf("Input student info:\n");
 	for(i = 0;i < pop;i++)
 	{
 		printf("STUDENT %d-----\n",i + 1);
 		printf("Enter name: ");
-		scanf("%s",&obj[i].name);
+		scanf("%s",&(obj + i)->name);
 		printf("Enter province: ");
-		scanf("%s",&obj[i].province);
+		scanf("%s",&(obj + i)->province);
 		printf("Enter birth year: ");
-		scanf("%d",&obj[i].birthYear);
+		scanf("%d",&(obj + i)->birthYear);
 		printf("Enter mark: ");
-		scanf("%f",&obj[i].mark);		
+		scanf("%f",&(obj + i)->mark);	
 	}
 }
 
 void printHeaders()
 {
+	printf("STUDENT INFORMATIONS\n");
 	printf("NO\tNAME\tPROVINCE\tBIRTH YEAR\tMARK\n");
 }
 
@@ -41,7 +43,7 @@ void printInfo()
 {
 	for(int i = 0;i < pop;i++)
 	{
-		printf("%d\t%s\t%s\t\t%d\t\t%.2f\n",i + 1,obj[i].name,obj[i].province,obj[i].birthYear,obj[i].mark);
+		printf("%d\t%s\t%s\t\t%d\t\t%.2f\n",i + 1,(obj + i)->name,(obj + i)->province,(obj + i)->birthYear,(obj + i)->mark);
 	}
 }
 
